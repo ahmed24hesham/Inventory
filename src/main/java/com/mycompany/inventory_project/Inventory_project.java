377ebbe5a07c020813af8c2097752f75c86a098b
Asus <Asus@192.168.1.5>
2025-10-16 23:38:03 +0300

    main

---
+++ src/main/java/com/mycompany/inventory_project/Inventory_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventory_project;
+
+/**
+ *
+ * @author Asus
+ */
+public class Inventory_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
