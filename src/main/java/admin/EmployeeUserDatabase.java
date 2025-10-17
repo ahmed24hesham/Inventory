621c3fae3837639fb17cfcaeb8037963aa1a279d
Mohamed <Mohamed@MSI>
2025-10-17 15:42:58 +0300

    Database Abstract class and his child EmployeeUserDatabase

--- src/main/java/admin/EmployeeUserDatabase.java
+++ src/main/java/admin/EmployeeUserDatabase.java
@@ -1,5 +1,62 @@
 package admin;
+import java.io.PrintWriter;
+import admin.*;
+import employee.DataBase;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class EmployeeUserDatabase extends DataBase<EmployeeUser>{
+    public EmployeeUserDatabase(String filename){
+        super(filename);
+    }
+    public void readFromFile() {
+        records.clear();
+        try (Scanner input = new Scanner(new File(filename))) {
+            while (input.hasNextLine()) {
+                String line = input.nextLine().trim();
+                if (!line.isEmpty()) {
+                    records.add(createRecordFrom(line));
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found: " + filename);
+        }
+    }
+       public EmployeeUser createRecordFrom(String line) {
+        String[] parts = line.split(",");
+        return new EmployeeUser(parts[0].trim(),parts[1].trim(),parts[2].trim(),parts[3].trim(),parts[4].trim());
+    }
+       public boolean contains(String key){
+      for(int i=0;i<records.size();i++){
+     if(records.get(i).getSearchKey().compareTo(key)==0)
+         return true;
+      }
+      return false;
+    }
+       public EmployeeUser getRecord(String key) {
+         for(int i=0;i<records.size();i++){
+     if(records.get(i).getSearchKey().compareTo(key)==0)
+         return records.get(i);
+      }
+      return null;
+       }
+       public void deleteRecord(String key){
+           for(int i=0;i<records.size();i++){
+     if(records.get(i).getSearchKey().compareTo(key)==0){
+         records.remove(i);
+         return;
+     }
+      }
+           System.out.println("Not found");
+       }
+   
+       public void saveToFile() throws FileNotFoundException  {
+    PrintWriter pr = new PrintWriter(filename);
+    for (int i = 0; i < records.size(); i++) {
+        pr.println(records.get(i).lineRepresentation());
+    }
+    pr.close();
+}
 
-public class EmployeeUserDatabase {
-
+       
 }
--- src/main/java/com/mycompany/inventory_project/Inventory_project.java
+++ src/main/java/com/mycompany/inventory_project/Inventory_project.java
@@ -5,6 +5,9 @@
 package com.mycompany.inventory_project;
 
 import admin.EmployeeUser;
+import admin.EmployeeUserDatabase;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,9 +15,23 @@ import admin.EmployeeUser;
  */
 public class Inventory_project {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        
         EmployeeUser e1=new EmployeeUser("E200","ahmed","ahmed@gmail.com","alex","012030646431");
          System.out.println(e1.lineRepresentation());
+        EmployeeUserDatabase b = new EmployeeUserDatabase("data/Employee.txt");
+          b.readFromFile();
+         boolean c= b.contains("E1200");
+         System.out.println(c);
+         EmployeeUser e2=b.createRecordFrom("E4000,Ahmed,ahmed_1999@gmail.com,Alexandria,01088877345");
+         EmployeeUser e3=b.createRecordFrom("E3,Ahmed,ahmed_1999@gmail.com,Alexandria,01088877345");
+          ArrayList<EmployeeUser> records = b.returnAllRecords();
+          System.out.println(b.contains("E1400"));
+          System.out.println(b.contains("E1000"));
+          b.insertRecord(e2);
+          b.insertRecord(e3);
+          b.saveToFile();
+                  
+         
     }
 }
