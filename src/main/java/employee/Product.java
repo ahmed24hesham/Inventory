6d0262e124b9f377704a6775db766a4e209d9bc8
Asus <Asus@192.168.1.5>
2025-10-18 05:53:39 +0300

    finished the product class

--- src/main/java/employee/Product.java
+++ src/main/java/employee/Product.java
@@ -1,5 +1,33 @@
 package employee;
 
 public class Product {
+private String productID;
+private String productName;
+private String manufacturerName;
+private String supplierName;
+private int quantity;
+private float price;
 
-}
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+    public int getQuantity(){
+        return this.quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+    public String lineRepresentation(){
+       return this.productID + "," + this.productName +"," +this.manufacturerName +"," +this.supplierName +","+this.quantity+","+this.price;
+    }
+    public String getSearchKey(){
+        return this.productID;
+    }
+    }
+
