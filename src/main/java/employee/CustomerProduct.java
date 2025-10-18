32246d1eb75a2e6f33ae66359b8b2c77e58c6e9d
eslamia <eslamia@DESKTOP-C08O5DK>
2025-10-18 13:37:15 +0300

    branch commit

--- src/main/java/employee/CustomerProduct.java
+++ src/main/java/employee/CustomerProduct.java
@@ -1,5 +1,40 @@
 package employee;
 
-public class CustomerProduct {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class CustomerProduct {
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+    
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate){
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = paid;   
+    }
+    
+    public String getCustomerSSN(){
+        return this.customerSSN;
+    }
+    
+    public String getProductID(){
+        return this.productID;
+    }
+    
+    public LocalDate getPurchaseDate(){
+        return this.purchaseDate;
+    }
+    
+    public String lineRepresentation(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return this.customerSSN + ',' + this.getCustomerSSN() + ',' + this.purchaseDate.format(formatter) + "," + this.paid ;
+    }
+    
+     public String getSearchKey() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return this.customerSSN + "," + this.productID + "," + this.purchaseDate.format(formatter);
+    }
 }
